814aa6a427c6501725d44b9ca6098be4c848d756
Kaylyn Bogle <kaylyn@kaylyn.ink>
2020-07-22 16:53:54 -0700

    Move from StandardJS to GNOME eslint configs

--- extension.js
+++ extension.js
@@ -16,70 +16,67 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-const { CursorTracker, IdleMonitor } = imports.gi.Meta
+const { CursorTracker, IdleMonitor } = imports.gi.Meta;
 
-const ExtensionUtils = imports.misc.extensionUtils
+const ExtensionUtils = imports.misc.extensionUtils;
 
-const HIDE_TIME_KEY = 'hide-time'
+const HIDE_TIME_KEY = 'hide-time';
 
 class Extension {
-  constructor () {
-  }
+    enable() {
+        this._settings = ExtensionUtils.getSettings();
 
-  enable () {
-    this._settings = ExtensionUtils.getSettings()
+        this._settings_connection = this._settings.connect(`changed::${HIDE_TIME_KEY}`, this._hideTimeChanged.bind(this));
 
-    this._settings_connection = this._settings.connect(`changed::${HIDE_TIME_KEY}`, this._hideTimeChanged.bind(this))
+        this._cursorTracker = CursorTracker.get_for_display(global.display);
+        this._idleMonitor = IdleMonitor.get_core();
 
-    this._cursorTracker = CursorTracker.get_for_display(global.display)
-    this._idleMonitor = IdleMonitor.get_core()
-
-    this._addIdleWatch(this._settings.get_double(HIDE_TIME_KEY))
-  }
-
-  disable () {
-    this._removeIdleWatch()
-
-    if (this._settings_connection) {
-      this._settings.disconnect(this._settings_connection)
-      this._settings_connection = null
+        this._addIdleWatch(this._settings.get_double(HIDE_TIME_KEY));
     }
 
-  }
+    disable() {
+        this._removeIdleWatch();
 
-  _hideTimeChanged () {
-    this._addIdleWatch(this._settings.get_double(HIDE_TIME_KEY))
-  }
+        if (this._settings_connection) {
+            this._settings.disconnect(this._settings_connection);
+            this._settings_connection = null;
+        }
 
-  _addIdleWatch (idleTime) {
-    this._removeIdleWatch()
-
-    this._idle_watch = this._idleMonitor.add_idle_watch(idleTime * 1000, this._onIdle.bind(this))
-  }
-
-  _removeIdleWatch () {
-    if (this._idle_watch) {
-      this._idleMonitor.remove_watch(this._idle_watch)
-      this._idle_watch = null
     }
 
-    if (this._active_watch) {
-      this._idleMonitor.remove_watch(this._active_watch)
-      this._active_watch = null
+    _hideTimeChanged() {
+        this._addIdleWatch(this._settings.get_double(HIDE_TIME_KEY));
     }
-  }
 
-  _onIdle () {
-    this._cursorTracker.set_pointer_visible(false)
+    _addIdleWatch(idleTime) {
+        this._removeIdleWatch();
 
-    this._active_watch = this._idleMonitor.add_user_active_watch(this._onActive.bind(this))
-  }
+        this._idle_watch = this._idleMonitor.add_idle_watch(idleTime * 1000, this._onIdle.bind(this));
+    }
 
-  _onActive () {
-    this._cursorTracker.set_pointer_visible(true)
-  }
+    _removeIdleWatch() {
+        if (this._idle_watch) {
+            this._idleMonitor.remove_watch(this._idle_watch);
+            this._idle_watch = null;
+        }
+
+        if (this._active_watch) {
+            this._idleMonitor.remove_watch(this._active_watch);
+            this._active_watch = null;
+        }
+    }
+
+    _onIdle() {
+        this._cursorTracker.set_pointer_visible(false);
+
+        this._active_watch = this._idleMonitor.add_user_active_watch(this._onActive.bind(this));
+    }
+
+    _onActive() {
+        this._cursorTracker.set_pointer_visible(true);
+    }
 }
 
-function init () { // eslint-disable-line no-unused-vars
-  return new Extension()
+function init() { // eslint-disable-line no-unused-vars
+    return new Extension();
 }
--- prefs.js
+++ prefs.js
@@ -16,43 +16,43 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-const Gtk = imports.gi.Gtk
+const Gtk = imports.gi.Gtk;
 
-const ExtensionUtils = imports.misc.extensionUtils
+const ExtensionUtils = imports.misc.extensionUtils;
 
-let settings
+let settings;
 
-function init () { // eslint-disable-line no-unused-vars
-  settings = ExtensionUtils.getSettings()
+function init() { // eslint-disable-line no-unused-vars
+    settings = ExtensionUtils.getSettings();
 }
 
-function buildPrefsWidget () { // eslint-disable-line no-unused-vars
-  const frame = new Gtk.VBox({ border_width: 10, spacing: 6 })
+function buildPrefsWidget() { // eslint-disable-line no-unused-vars
+    const frame = new Gtk.VBox({ border_width: 10, spacing: 6 });
 
-  frame.pack_start(new Gtk.Label({
-    label: '<b>Timing</b>',
-    use_markup: true,
-    xalign: 0
-  }), false, false, 0)
+    frame.pack_start(new Gtk.Label({
+        label: '<b>Timing</b>',
+        use_markup: true,
+        xalign: 0,
+    }), false, false, 0);
 
-  const sectionVbox = new Gtk.VBox({ margin_left: 15, margin_top: 8, margin_bottom: 8 })
-  const spinner = Gtk.SpinButton.new_with_range(0.0, 600.0, 0.5)
-  spinner.set_value(settings.get_double('hide-time'))
-  const sectionHbox = new Gtk.HBox()
-  sectionHbox.pack_start(new Gtk.Label({
-    label: 'Delay in seconds before hiding cursor',
-    use_markup: true,
-    xalign: 0
-  }), true, true, 0)
-  sectionHbox.pack_end(spinner, false, false, 0)
+    const sectionVbox = new Gtk.VBox({ margin_left: 15, margin_top: 8, margin_bottom: 8 });
+    const spinner = Gtk.SpinButton.new_with_range(0.0, 600.0, 0.5);
+    spinner.set_value(settings.get_double('hide-time'));
+    const sectionHbox = new Gtk.HBox();
+    sectionHbox.pack_start(new Gtk.Label({
+        label: 'Delay in seconds before hiding cursor',
+        use_markup: true,
+        xalign: 0,
+    }), true, true, 0);
+    sectionHbox.pack_end(spinner, false, false, 0);
 
-  sectionVbox.pack_start(sectionHbox, false, false, 0)
+    sectionVbox.pack_start(sectionHbox, false, false, 0);
 
-  settings.connect('changed::hide-time', (_, value) => spinner.set_value(settings.get_double(value)))
-  spinner.connect('value-changed', widget => settings.set_double('hide-time', widget.get_value()))
+    settings.connect('changed::hide-time', (_, value) => spinner.set_value(settings.get_double(value)));
+    spinner.connect('value-changed', widget => settings.set_double('hide-time', widget.get_value()));
 
-  frame.pack_start(sectionVbox, false, false, 0)
+    frame.pack_start(sectionVbox, false, false, 0);
 
-  frame.show_all()
-  return frame
+    frame.show_all();
+    return frame;
 }
